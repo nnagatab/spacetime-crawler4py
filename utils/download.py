e32aff64d02f243a96b68891578f06e421f63342
nnagatab <nnagatab@uci.edu>
2021-01-27 20:52:30 -0800

    make sure pages aren't empty

--- utils/download.py
+++ utils/download.py
@@ -8,8 +8,8 @@ def download(url, config, logger=None):
     host, port = config.cache_server
     resp = requests.get(
         f"http://{host}:{port}/",
-        params=[("q", f"{url}"), ("u", f"{config.user_agent}")])
-    if resp:
+        params=[("q", f"{url}"), ("u", f"{config.user_agent}")], timeout = 3)
+    if resp and resp.content:  #added to make sure page has content
         return Response(cbor.loads(resp.content))
     logger.error(f"Spacetime Response error {resp} with url {url}.")
     return Response({
